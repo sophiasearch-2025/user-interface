165307c0f6d54c62f71fca1fe357d55c13648814
Jonagu1 <j.aguerofica@outlook.es>
2025-11-06 10:40:47 -0300

    Creacion del componente Footer para la pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="bg-background min-h-screen">
+    <main className="bg-background min-h-screen flex-col flex justify-between">
       <Navbar />
+      <Footer />
     </main>
   );
 }
