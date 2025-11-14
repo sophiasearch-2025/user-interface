1d190c15f32dc65c8c9074a97111ddb7c59b36f3
Cristóbal Veas <cristobal.veas@proton.me>
2025-11-14 17:35:28 -0300

    Crear componente AuthButtons

---
+++ components/AuthButtons.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import Register from "./Register";
+import Login from "./Login";
+
+type AuthButtonsProps = {
+  showRegister?: boolean;
+  showLogin?: boolean;
+};
+
+export default function AuthButtons({ showRegister, showLogin }: AuthButtonsProps) {
+  const [modalActivo, setModalActivo] = useState<"login" | "register" | null>(null);
+
+  return (
+    <div className="flex items-center gap-8">
+      <Register
+        className={showRegister ? `` : `hidden`}
+        isOpen={modalActivo === "register"}
+        onOpen={() => setModalActivo("register")}
+        onClose={() => setModalActivo(null)}
+        onSwitch={() => setModalActivo("login")}
+      />
+
+      <Login
+        className={showLogin ? `` : `hidden`}
+        isOpen={modalActivo === "login"}
+        onOpen={() => setModalActivo("login")}
+        onClose={() => setModalActivo(null)}
+        onSwitch={() => setModalActivo("register")}
+      />
+    </div>
+  );
+}
