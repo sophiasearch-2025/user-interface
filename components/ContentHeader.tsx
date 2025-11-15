6cb53ce5af316a6f94abaac4b33340cb4bf5f077
Cristóbal Veas <cristobal.veas@proton.me>
2025-11-15 00:02:46 -0300

    Modificar ContentHeader para hacerlo más dinámico

--- components/ContentHeader.tsx
+++ components/ContentHeader.tsx
@@ -1,23 +1,18 @@
 import Title from "@/components/Title";
+import { ReactNode } from "react";
 
 type ContentHeaderProps = {
   firstLine: string;
   secondLine: string;
+  children: ReactNode;
 };
 
-export default function ContentHeader({ firstLine, secondLine }: ContentHeaderProps) {
+export default function ContentHeader({ firstLine, secondLine, children }: ContentHeaderProps) {
   return (
     <div className="flex justify-between items-center mx-auto py-4">
       <Title firstLine={firstLine} secondLine={secondLine} />
 
-      <div className="flex items-center gap-3 px-8">
-        <p className="text-sm text-text-muted">Mostrar por página:</p>
-
-        <select className="bg-surface-dark border border-border-primary rounded-md p-1 text-sm text-foreground focus:outline-none focus:border-border-primary">
-          <option value="4">4</option>
-          <option value="6">6</option>
-        </select>
-      </div>
+      {children}
     </div>
   );
 }
