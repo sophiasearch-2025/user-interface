ccc658ae9d73fc5d6e8d93b9209363f1a9a5ee19
Cristóbal Veas <cristobal.veas@proton.me>
2025-11-18 07:43:31 -0300

    Implementar tipos de respuesta de la API y adaptar tipos actuales

--- types/index.ts
+++ types/index.ts
@@ -2,16 +2,53 @@ export type NewsItem = {
   id: string;
   title: string;
   date: string;
-  author: string;
   url: string;
   source: string;
+  country: string;
+  author: string;
   category: string;
 };
 
+export type ApiNewsItem = {
+  id: number;
+  title: string;
+  date: string;
+  fecha: string;
+  media_outlet: string;
+  url: string;
+  text: string;
+  country: string;
+  autor: string;
+  categoria: string;
+  palabrasClave: string[];
+};
+
+export type ApiResponse = {
+  success: boolean;
+  data: ApiNewsItem[];
+  paginacion: {
+    total: number;
+    pagina: number;
+    limite: number;
+    totalPaginas: number;
+  };
+};
+
+export type FiltersApiResponse = {
+  success: boolean;
+  data: {
+    media_outlet: string[];
+    country: string[];
+    categoria: string[];
+    autor: string[];
+  };
+};
+
 export type NewsFilterState = {
+  searchTerm: string;
   startDate: Date | null;
   endDate: Date | null;
   media: string[];
   categories: string[];
-  // Aquí podríamos añadir 'searchTerm' en el futuro
+  authors: string[];
 };
